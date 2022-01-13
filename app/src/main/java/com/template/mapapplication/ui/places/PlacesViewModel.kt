7c1b74d41c8c197e30fc1e49e7baa6b7e0c6ab5d
Vladimir <Shifman.1704@mail.ru>
2022-01-13 15:37:10 +0700

    code style & liveData changed

--- app/src/main/java/com/template/mapapplication/ui/places/PlacesViewModel.kt
+++ app/src/main/java/com/template/mapapplication/ui/places/PlacesViewModel.kt
@@ -6,8 +6,5 @@ import androidx.lifecycle.ViewModel
 
 class PlacesViewModel : ViewModel() {
 
-    private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
-    }
-    val text: LiveData<String> = _text
+    val text = MutableLiveData("This is Places Fragment")
 }
