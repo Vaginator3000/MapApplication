23d275ffd3a38d93e2eea12ab69bd5b56ff82fad
Vladimir <Shifman.1704@mail.ru>
2022-01-28 16:36:49 +0700

    add registration code

--- data/src/main/java/com/template/data/login/SharedPrefsDB.kt
+++ data/src/main/java/com/template/data/login/SharedPrefsDB.kt
@@ -2,10 +2,43 @@ package com.template.data.login
 
 import android.content.Context
 import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.template.models.LoginUserModel
 
 class SharedPrefsDB(context: Context) {
     private val sPrefs = context.getSharedPreferences("data", Context.MODE_PRIVATE)
     private val gson = Gson()
 
-    //логика авторизации и регистрации через префы будет тут
+    fun isUserExist(login: String, email: String) : Boolean {
+        getAllUsers().forEach { user ->
+            if (user.email == email || user.login == login)
+                return true
+        }
+        return false
+    }
+
+    fun addUser(user: LoginUserModel) {
+        var users = getAllUsers()
+        if (users.isEmpty())
+            users = listOf(user)
+        else {
+            users as MutableList
+            users.add(user)
+        }
+        saveUsers(users)
+    }
+
+    private fun getAllUsers(): List<LoginUserModel> {
+        val listInStr = sPrefs.getString("users", null) ?: return listOf()
+
+        val type = object : TypeToken<List<LoginUserModel>>() {}.type
+        val users: List<LoginUserModel> = gson.fromJson(listInStr, type)
+        return  users
+    }
+
+    private fun saveUsers(users: List<LoginUserModel>) {
+        sPrefs.edit()
+            .putString("users", gson.toJson(users))
+            .apply()
+    }
 }
