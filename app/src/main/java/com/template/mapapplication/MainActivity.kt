c558f67ab24c6f8ee07a9fdb903150f0859c716f
Vladimir <Shifman.1704@mail.ru>
2022-02-11 00:08:35 +0700

    add map with basics methods

--- app/src/main/java/com/template/mapapplication/MainActivity.kt
+++ app/src/main/java/com/template/mapapplication/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.template.mapapplication.databinding.ActivityMainBinding
+import com.yandex.mapkit.MapKitFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -31,5 +32,8 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        //Чтоб не заливать ключ на гит
+        MapKitFactory.setApiKey(KeyClass().key)
     }
 }
