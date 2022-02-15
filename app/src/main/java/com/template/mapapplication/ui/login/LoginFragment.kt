a83f19b7ed052c27638313b89e0b2e7b0db7cd17
Vladimir <Shifman.1704@mail.ru>
2022-02-15 14:01:20 +0700

    login code & make loginFragment as first loading fragment also: remove wrong sting & remove padding in mainActivity & replace binding in mainActivity to viewBindingDelegate

--- app/src/main/java/com/template/mapapplication/ui/login/LoginFragment.kt
+++ app/src/main/java/com/template/mapapplication/ui/login/LoginFragment.kt
@@ -2,13 +2,14 @@ package com.template.mapapplication.ui.login
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.template.mapapplication.R
 import com.template.mapapplication.databinding.FragmentLoginBinding
+import com.template.models.LoginUserModel
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
     private val binding: FragmentLoginBinding by viewBinding(FragmentLoginBinding::bind)
@@ -17,9 +18,86 @@ class LoginFragment : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        loginViewModel.text.observe(viewLifecycleOwner, Observer {
-        
-        })
+        setAuthMode()
+        setOnClicks()
+    }
+
+    private fun setOnClicks() {
+        with(binding) {
+            registrationBtn.setOnClickListener {
+                setRegistrationMode()
+            }
+
+            authBtn.setOnClickListener {
+                setAuthMode()
+            }
+
+            continueAuthBtn.setOnClickListener {
+                val loginOrEmail = (loginOrEmailEditText.text ?: return@setOnClickListener).toString().trim()
+                val password = (passwordEditText.text ?: return@setOnClickListener).toString()
+
+                if (!checkFieldsAreCorrectAndNotEmpty()) return@setOnClickListener
+
+                if (loginViewModel.authenticate(loginOrEmail = loginOrEmail, password = password)) {
+                    // navigateToNextFragment()
+                } else
+                    showToast(getString(R.string.incorrect_login_or_password))
+            }
+
+            continueRegBtn.setOnClickListener {
+                val login = (loginEditText.text ?: return@setOnClickListener).toString().trim()
+                val email = (emailEditText.text ?: return@setOnClickListener).toString().trim()
+                val pass1 = (pass1EditText.text ?: return@setOnClickListener).toString()
+
+                if (!checkFieldsAreCorrectAndNotEmpty()) return@setOnClickListener
+
+                if (loginViewModel.checkUserAlreadyExist(login = login, email = email)) {
+                    showToast(message = getString(R.string.user_already_exists))
+                    return@setOnClickListener
+                }
+
+                loginViewModel.addUser(
+                    user = LoginUserModel(
+                        login = login,
+                        password = pass1,
+                        email = email
+                    )
+                )
+                // navigateToNextFragment()
+            }
+        }
+    }
+
+    private fun checkFieldsAreCorrectAndNotEmpty() : Boolean {
+        with(binding) {
+            if (registrationGroup.isVisible) {
+                if (loginEditText.text.toString().isEmpty() || emailEditText.text.toString().isEmpty() ||
+                    pass1EditText.text.toString().isEmpty() || pass2EditText.text.toString().isEmpty()  ) {
+                    showToast(message = getString(R.string.fields_cant_be_empty))
+                    return false
+                }
+                if (!acceptSwitch.isChecked) {
+                    showToast(message = getString(R.string.check_privacy_policy))
+                    return false
+                }
+                if (pass1EditText.text.toString() != pass2EditText.text.toString()) {
+                    showToast(message = getString(R.string.passwords_are_different))
+                    return false
+                }
+            }
+
+            if (authenticationGroup.isVisible) {
+                if (loginOrEmailEditText.text.toString().isEmpty() || passwordEditText.text.toString().isEmpty()) {
+                    showToast(message = getString(R.string.fields_cant_be_empty))
+                    return false
+                }
+            }
+        }
+        return true
+    }
+
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 
     private fun setRegistrationMode() {
--- app/src/main/java/com/template/mapapplication/ui/login/LoginViewModel.kt
+++ app/src/main/java/com/template/mapapplication/ui/login/LoginViewModel.kt
@@ -1,8 +1,24 @@
 package com.template.mapapplication.ui.login
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.template.data.login.SharedPrefsAuthenticationImpl
+import com.template.data.login.SharedPrefsRegistrationImpl
+import com.template.models.LoginUserModel
 
-class LoginViewModel : ViewModel() {
-    val text = MutableLiveData("This is Login Fragment")
+class LoginViewModel(app: Application) : AndroidViewModel(app) {
+    private val sharedPrefsAuthImpl by lazy { SharedPrefsAuthenticationImpl(context = getApplication<Application>().applicationContext) }
+    private val sharedPrefsRegImpl by lazy { SharedPrefsRegistrationImpl(context = getApplication<Application>().applicationContext) }
+
+    fun authenticate(loginOrEmail: String, password: String) : Boolean {
+        return sharedPrefsAuthImpl.authenticate(loginOrEmail = loginOrEmail, password = password)
+    }
+
+    fun addUser(user: LoginUserModel) {
+        sharedPrefsRegImpl.addUser(user = user)
+    }
+
+    fun checkUserAlreadyExist(login: String, email: String) : Boolean {
+        return sharedPrefsRegImpl.isUserExist(login, email)
+    }
 }
