b201da1ae25bfd6159a1096f9ad1ea966abf3b8d
Vladimir <Shifman.1704@mail.ru>
2022-02-16 01:29:16 +0700

    navigation from login to map & disabled return to login by pressing back button

--- app/src/main/java/com/template/mapapplication/MainActivity.kt
+++ app/src/main/java/com/template/mapapplication/MainActivity.kt
@@ -3,6 +3,7 @@ package com.template.mapapplication
 import android.os.Bundle
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.isVisible
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -21,19 +22,34 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val navView: BottomNavigationView = binding.navView
-
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_map, R.id.navigation_places
             )
         )
+
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
 
+        //Скрываю таббар на экране авторизации
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            navView.isVisible =
+                if (destination.id == R.id.navigation_login) false
+                else true
+        }
+
         //Чтоб не заливать ключ на гит
         MapKitFactory.setApiKey(KeyClass().key)
+    }
+
+    //Избегаю возвращения на фрагмент с авторизацией
+    override fun onBackPressed() {
+        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        if (navController.currentDestination?.id == R.id.navigation_login)
+            super.onBackPressed()
+        else
+            finish()
     }
 }
